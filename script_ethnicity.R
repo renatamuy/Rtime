80c4cbb8929a192491994a15fc6f054c26d03154
Renata Muylaert <renatamuy@gmail.com>
2020-08-13 10:52:36 +1200

    Add files via upload

---
+++ script_ethnicity.R
@@ -0,0 +1,9 @@
+# Sorting a presence value as a factor in a new columns
+
+df<- data.frame(japanese = c(1,0,0), kiwi=c(0,1,2))
+
+df
+
+df$ethnicity <- ifelse(df$japanese >= 1, "japanese", ifelse(df$kiwi >= 1, "kiwi", "not declared"))
+
+df$ethnicity
