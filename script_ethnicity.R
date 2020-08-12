4172c9acf0e3bf1f34bac51f0b59d7a58cb4c313
Renata Muylaert <renatamuy@gmail.com>
2020-08-13 10:54:09 +1200

    Update script_ethnicity.R

--- script_ethnicity.R
+++ script_ethnicity.R
@@ -4,6 +4,6 @@ df<- data.frame(japanese = c(1,0,0), kiwi=c(0,1,2))
 
 df
 
-df$ethnicity <- ifelse(df$japanese >= 1, "japanese", ifelse(df$kiwi >= 1, "kiwi", "not declared"))
+df$ethnicity <- ifelse(df$japanese == 1, "japanese", ifelse(df$kiwi == 1, "kiwi", "not declared"))
 
 df$ethnicity
